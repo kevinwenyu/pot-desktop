6166cbd999a380bdead46b4de158c72b84383cd5
kevinwenyu <34227344+kevinwenyu@users.noreply.github.com>
2026-01-17 14:21:40 +0100

    Update language.ts
    add cs and hu languege

--- src/utils/language.ts
+++ src/utils/language.ts
@@ -31,6 +31,8 @@ export const languageList = [
     'nl',
     'uk',
     'he',
+    'cs',
+    'hu',
 ];
 
 // https://flagicons.lipis.dev/
@@ -65,4 +67,6 @@ export enum LanguageFlag {
     nl = 'nl',
     uk = 'ua',
     he = 'il',
+    cs = 'cz', // 捷克国旗代码是 cz
+    hu = 'hu', // 匈牙利国旗代码是 hu
 }
